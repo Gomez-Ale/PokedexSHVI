26732ae0727b52e18d0e948525d97c82131597fe
Gomez-Ale <alegomez001122@gmail.com>
2020-02-02 03:58:52 -0500

    Added various table renderers

--- app/src/main/java/com/alejandro/pokedex/Example.java
+++ app/src/main/java/com/alejandro/pokedex/Example.java
@@ -1,13 +1,23 @@
-//package com.alejandro.pokedex;
-//
-//import me.sargunvohra.lib.pokekotlin.client.PokeApi;
-//import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
-//import me.sargunvohra.lib.pokekotlin.model.Pokemon;
-//import me.sargunvohra.lib.pokekotlin.model.PokemonSpecies;
-//
-////public class Example {
-//    public static void main(String args[]) {
-//
+package com.alejandro.pokedex;
+
+import me.sargunvohra.lib.pokekotlin.client.PokeApi;
+import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
+import me.sargunvohra.lib.pokekotlin.model.EggGroup;
+import me.sargunvohra.lib.pokekotlin.model.Pokemon;
+import me.sargunvohra.lib.pokekotlin.model.PokemonSpecies;
+import me.sargunvohra.lib.pokekotlin.model.Type;
+
+public class Example {
+
+    public static void main(String[] args) {
+        PokeApi pokeApi = new PokeApiClient();
+
+        EggGroup[] pokeTypes = new EggGroup[151];
+        for (int n = 0; n <= 150; n++) {
+            pokeTypes[n] = pokeApi.getEggGroup(n+1);
+            System.out.println(pokeTypes[n]);
+
+        }
 //        PokeApi pokeApi = new PokeApiClient();
 //        Pokemon[] pokedex1 = new Pokemon[151];
 //        for(int n=0; n <=150; n++)
@@ -30,98 +40,36 @@
 ////            System.out.println(pokedex3[n]);
 ////            System.out.println();
 //        }
-//        Pokemon[] pokedex4 = new Pokemon[106];
-//        for(int n=0; n <=105; n++)
+//        Pokemon[] pokedex4 = new Pokemon[107];
+//        for(int n=0; n <=106; n++)
 //        {
 //            pokedex4[n] = pokeApi.getPokemon(n+387);
-////            System.out.println(pokedex4[n]);
-////            System.out.println();
+//            System.out.println(pokedex4[n]);
+//            System.out.println();
 //        }
-//        Pokemon[] pokedex5 = new Pokemon[155];
-//        for(int n=0; n <=154; n++)
+//        Pokemon[] pokedex5 = new Pokemon[156];
+//        for(int n=0; n <=155; n++)
 //        {
 //            pokedex5[n] = pokeApi.getPokemon(n+494);
-////            System.out.println(pokedex5[n]);
-////            System.out.println();
+//            System.out.println(pokedex5[n]);
+//            System.out.println();
 //        }
-//        Pokemon[] pokedex6 = new Pokemon[71];
-//        for(int n=0; n <=70; n++)
+//        Pokemon[] pokedex6 = new Pokemon[72];
+//        for(int n=0; n <=71; n++)
 //        {
-//            pokedex5[n] = pokeApi.getPokemon(n+650);
-////            System.out.println(pokedex6[n]);
-////            System.out.println();
+//            pokedex6[n] = pokeApi.getPokemon(n+650);
+//            System.out.println(pokedex6[n]);
+//            System.out.println();
 //        }
-//        Pokemon[] pokedex7 = new Pokemon[85];
-//        for(int n=0; n <=84; n++)
+//        Pokemon[] pokedex7 = new Pokemon[86];
+//        for(int n=0; n <=85; n++)
 //        {
-//            pokedex5[n] = pokeApi.getPokemon(n+722);
-////            System.out.println(pokedex7[n]);
-////            System.out.println();
+//            pokedex7[n] = pokeApi.getPokemon(n+722);
+//            System.out.println(pokedex7[n]);
+//            System.out.println();
 //        }
-//    }
-////}
-package com.alejandro.pokedex;
-
-import me.sargunvohra.lib.pokekotlin.client.PokeApi;
-import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
-import me.sargunvohra.lib.pokekotlin.model.Pokemon;
-import me.sargunvohra.lib.pokekotlin.model.PokemonSpecies;
-
-public class Example {
-
-    public static void main(String[] args) {
-        PokeApi pokeApi = new PokeApiClient();
-        Pokemon[] pokedex1 = new Pokemon[151];
-        for(int n=0; n <=150; n++)
-        {
-            pokedex1[n] = pokeApi.getPokemon(n+1);
-//            System.out.println(pokedex1[n]);
-//            System.out.println();
-        }
-        Pokemon[] pokedex2 = new Pokemon[100];
-        for(int n=0; n <=99; n++)
-        {
-            pokedex2[n] = pokeApi.getPokemon(n+152);
-//            System.out.println(pokedex2[n]);
-//            System.out.println();
-        }
-        Pokemon[] pokedex3 = new Pokemon[135];
-        for(int n=0; n <=134; n++)
-        {
-            pokedex3[n] = pokeApi.getPokemon(n+252);
-//            System.out.println(pokedex3[n]);
-//            System.out.println();
-        }
-        Pokemon[] pokedex4 = new Pokemon[107];
-        for(int n=0; n <=106; n++)
-        {
-            pokedex4[n] = pokeApi.getPokemon(n+387);
-            System.out.println(pokedex4[n]);
-            System.out.println();
-        }
-        Pokemon[] pokedex5 = new Pokemon[156];
-        for(int n=0; n <=155; n++)
-        {
-            pokedex5[n] = pokeApi.getPokemon(n+494);
-            System.out.println(pokedex5[n]);
-            System.out.println();
-        }
-        Pokemon[] pokedex6 = new Pokemon[72];
-        for(int n=0; n <=71; n++)
-        {
-            pokedex6[n] = pokeApi.getPokemon(n+650);
-            System.out.println(pokedex6[n]);
-            System.out.println();
-        }
-        Pokemon[] pokedex7 = new Pokemon[86];
-        for(int n=0; n <=85; n++)
-        {
-            pokedex7[n] = pokeApi.getPokemon(n+722);
-            System.out.println(pokedex7[n]);
-            System.out.println();
-        }
-        int i = 0;
-
+//        int i = 0;
+//
 
     }
     public static Pokemon[] getDex(int gen) {
@@ -134,61 +82,117 @@ public class Example {
                 Pokemon[] pokedex1 = new Pokemon[151];
                 for (int n = 0; n <= 150; n++) {
                     pokedex1[n] = pokeApi.getPokemon(n + 1);
-//            System.out.println(pokedex1[n]);
-//            System.out.println();
+
                 }
                 return pokedex1;
             case 2:
                 Pokemon[] pokedex2 = new Pokemon[100];
                 for (int n = 0; n <= 99; n++) {
                     pokedex2[n] = pokeApi.getPokemon(n + 152);
-//            System.out.println(pokedex2[n]);
-//            System.out.println();
+
                 }
                 return pokedex2;
             case 3:
                 Pokemon[] pokedex3 = new Pokemon[135];
                 for (int n = 0; n <= 134; n++) {
                     pokedex3[n] = pokeApi.getPokemon(n + 252);
-//            System.out.println(pokedex3[n]);
-//            System.out.println();
+
                 }
                 return pokedex3;
             case 4:
                 Pokemon[] pokedex4 = new Pokemon[107];
                 for (int n = 0; n <= 106; n++) {
                     pokedex4[n] = pokeApi.getPokemon(n + 387);
-//                    System.out.println(pokedex4[n]);
-//                    System.out.println();
+
                 }
                 return pokedex4;
             case 5:
                 Pokemon[] pokedex5 = new Pokemon[156];
                 for (int n = 0; n <= 155; n++) {
                     pokedex5[n] = pokeApi.getPokemon(n + 494);
-//                    System.out.println(pokedex5[n]);
-//                    System.out.println();
+
                 }
                 return pokedex5;
             case 6:
                 Pokemon[] pokedex6 = new Pokemon[72];
                 for (int n = 0; n <= 71; n++) {
                     pokedex6[n] = pokeApi.getPokemon(n + 650);
-                    System.out.println(pokedex6[n]);
-                    System.out.println();
+
                 }
                 return pokedex6;
             case 7:
                 Pokemon[] pokedex7 = new Pokemon[86];
                 for (int n = 0; n <= 85; n++) {
                     pokedex7[n] = pokeApi.getPokemon(n + 722);
-                    System.out.println(pokedex7[n]);
-                    System.out.println();
+
                 }
                 return pokedex7;
             default:
                 break;
         }
-                return safeTest;
+        return safeTest;
+    }
+
+
+
+    public static Type[] getTypes(int gen) {
+        PokeApi pokeApi = new PokeApiClient();
+        Type[] safeTest = new Type[1];
+
+
+        switch (gen) {
+            case 1:
+                Type[] pokedex1 = new Type[151];
+                for (int n = 0; n <= 150; n++) {
+                    pokedex1[n] = pokeApi.getType(n + 1);
+
+                }
+                return pokedex1;
+            case 2:
+                Type[] pokedex2 = new Type[100];
+                for (int n = 0; n <= 99; n++) {
+                    pokedex2[n] = pokeApi.getType(n + 152);
+
+                }
+                return pokedex2;
+            case 3:
+                Type[] pokedex3 = new Type[135];
+                for (int n = 0; n <= 134; n++) {
+                    pokedex3[n] = pokeApi.getType(n + 252);
+
+                }
+                return pokedex3;
+            case 4:
+                Type[] pokedex4 = new Type[107];
+                for (int n = 0; n <= 106; n++) {
+                    pokedex4[n] = pokeApi.getType(n + 387);
+
+                }
+                return pokedex4;
+            case 5:
+                Type[] pokedex5 = new Type[156];
+                for (int n = 0; n <= 155; n++) {
+                    pokedex5[n] = pokeApi.getType(n + 494);
+
+                }
+                return pokedex5;
+            case 6:
+                Type[] pokedex6 = new Type[72];
+                for (int n = 0; n <= 71; n++) {
+                    pokedex6[n] = pokeApi.getType(n + 650);
+
+                }
+                return pokedex6;
+            case 7:
+                Type[] pokedex7 = new Type[86];
+                for (int n = 0; n <= 85; n++) {
+                    pokedex7[n] = pokeApi.getType(n + 722);
+
+                }
+                return pokedex7;
+            default:
+                break;
         }
+        return safeTest;
+    }
     }
--- app/src/main/java/com/alejandro/pokedex/MainActivity.java
+++ app/src/main/java/com/alejandro/pokedex/MainActivity.java
@@ -1,160 +1,84 @@
 package com.alejandro.pokedex;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Gravity;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-import me.sargunvohra.lib.pokekotlin.client.PokeApi;
-import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
+
+
+
 import me.sargunvohra.lib.pokekotlin.model.Pokemon;
 import me.sargunvohra.lib.pokekotlin.model.PokemonSpecies;
+import me.sargunvohra.lib.pokekotlin.model.Type;
+
 
 public class MainActivity extends AppCompatActivity {
 
-//    public void dexGen() {
-//        PokeApi pokeApi = new PokeApiClient();
-//        Pokemon[] pokedex1 = new Pokemon[160];
-//        for(int n=0; n <=150; n++)
-//        {
-//            pokedex1[n] = pokeApi.getPokemon(n+1);
-////            System.out.println(pokedex1[n]);
-////            System.out.println();
-//        }
-//        Pokemon[] pokedex2 = new Pokemon[100];
-//        for(int n=0; n <=99; n++)
-//        {
-//            pokedex2[n] = pokeApi.getPokemon(n+152);
-////            System.out.println(pokedex2[n]);
-////            System.out.println();
-//        }
-//        Pokemon[] pokedex3 = new Pokemon[135];
-//        for(int n=0; n <=134; n++)
-//        {
-//            pokedex3[n] = pokeApi.getPokemon(n+252);
-////            System.out.println(pokedex3[n]);
-////            System.out.println();
-//        }
-//        Pokemon[] pokedex4 = new Pokemon[106];
-//        for(int n=0; n <=105; n++)
-//        {
-//            pokedex4[n] = pokeApi.getPokemon(n+387);
-////            System.out.println(pokedex4[n]);
-////            System.out.println();
-//        }
-//        Pokemon[] pokedex5 = new Pokemon[155];
-//        for(int n=0; n <=154; n++)
-//        {
-//            pokedex5[n] = pokeApi.getPokemon(n+494);
-////            System.out.println(pokedex5[n]);
-////            System.out.println();
-//        }
-//        Pokemon[] pokedex6 = new Pokemon[71];
-//        for(int n=0; n <=70; n++)
-//        {
-//            pokedex6[n] = pokeApi.getPokemon(n+650);
-////            System.out.println(pokedex6[n]);
-////            System.out.println();
-//        }
-//        Pokemon[] pokedex7 = new Pokemon[85];
-//        for(int n=0; n <=84; n++)
-//        {
-//            pokedex7[n] = pokeApi.getPokemon(n+722);
-////            System.out.println(pokedex7[n]);
-////            System.out.println();
-//        }
-////        init(pokedex1,pokedex2,pokedex3,pokedex4,pokedex5,pokedex6,pokedex7);
-//    }
-
 
     public void init(){
-//        Pokemon[] pokedex1, Pokemon[] pokedex2, Pokemon[] pokedex3, Pokemon[] pokedex4, Pokemon[] pokedex5, Pokemon[] pokedex6, Pokemon[] pokedex7
         TableLayout stk = (TableLayout) findViewById(R.id.table_main);
-
-        TableRow tbrow0 = new TableRow(this);
-        TextView tv0 = new TextView(this);
-        tv0.setText(" Sl.No ");
-        tv0.setTextColor(Color.WHITE);
-        tbrow0.addView(tv0);
-        TextView tv1 = new TextView(this);
-        tv1.setText(" Product ");
-        tv1.setTextColor(Color.WHITE);
-        tbrow0.addView(tv1);
-        TextView tv2 = new TextView(this);
-        tv2.setText(" Unit Price ");
-        tv2.setTextColor(Color.WHITE);
-        tbrow0.addView(tv2);
-        TextView tv3 = new TextView(this);
-        tv3.setText(" Stock Remaining ");
-        tv3.setTextColor(Color.WHITE);
-        tbrow0.addView(tv3);
-        stk.addView(tbrow0);
-//        Pokemon[] pokedex1 = Example.getDex(1);
-        for(int n=0; n <=150; n++) {
-            TableRow tbrow = new TableRow(this);
-            TextView t1v = new TextView(this);
-            t1v.setText("");
-            t1v.setTextColor(Color.WHITE);
-            t1v.setGravity(Gravity.CENTER);
-            tbrow.addView(t1v);
-            TextView t2v = new TextView(this);
-            t2v.setText("Product " + n);
-            t2v.setTextColor(Color.WHITE);
-            t2v.setGravity(Gravity.CENTER);
-            tbrow.addView(t2v);
-            TextView t3v = new TextView(this);
-            t3v.setText("Rs." + n);
-            t3v.setTextColor(Color.WHITE);
-            t3v.setGravity(Gravity.CENTER);
-            tbrow.addView(t3v);
-            TextView t4v = new TextView(this);
-            t4v.setText("" + n * 15 / 32 * 10);
-            t4v.setTextColor(Color.WHITE);
-            t4v.setGravity(Gravity.CENTER);
-            tbrow.addView(t4v);
-            stk.addView(tbrow);
+        for(int i = 1; i <= 7; i++) {
+            TableRow tbrow0 = new TableRow(this);
+            TextView t0v = new TextView(this);
+            t0v.setText("Generation " + i);
+            t0v.setTextColor(Color.WHITE);
+            t0v.setTextSize(40);
+            t0v.setGravity(Gravity.CENTER);
+            tbrow0.addView(t0v);
+            stk.addView(tbrow0);
+            TableRow tbrow1 = new TableRow(this);
+            TextView tv0 = new TextView(this);
+            tv0.setText(" Pokemon");
+            tv0.setTextColor(Color.WHITE);
+            tbrow1.addView(tv0);
+            TextView tv2 = new TextView(this);
+            tv2.setText(" Dex Number ");
+            tv2.setTextColor(Color.WHITE);
+            tbrow1.addView(tv2);
+            TextView tv3 = new TextView(this);
+            tv3.setText("     ");
+            tv3.setTextColor(Color.WHITE);
+            tbrow1.addView(tv3);
+            stk.addView(tbrow1);
+            Pokemon[] pokeList= Example.getDex(i);
+            for (int n = 0; n < pokeList.length; n++) {
+                TableRow tbrow = new TableRow(this);
+                TextView t1v = new TextView(this);
+                t1v.setPadding(25, 2, 0, 23);
+                t1v.setText(pokeList[n].getName().substring(0, 1).toUpperCase() + pokeList[n].getName().substring(1).toLowerCase());
+                t1v.setTextColor(Color.WHITE);
+                t1v.setGravity(Gravity.LEFT);
+                tbrow.addView(t1v);
+                TextView t2v = new TextView(this);
+                t2v.setText("Dex#: " + pokeList[n].getId());
+                t2v.setTextColor(Color.WHITE);
+                t2v.setGravity(Gravity.LEFT);
+                tbrow.addView(t2v);
+//                Image I3v = pokeList[n].getSprites().getFrontDefault();
+//                I3v.
+//                t3v.setTextColor(Color.WHITE);
+//                t3v.setGravity(Gravity.CENTER);
+//                tbrow.addView(t3v);
+                stk.addView(tbrow);
+            }
         }
-//        for(int n=0; n <=99; n++) {
-//            TableRow tbrow = new TableRow(this);
-//            TextView t1v = new TextView(this);
-//            t1v.setText(pokedex2[n]+ "");
-//            t1v.setTextColor(Color.WHITE);
-//            t1v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t1v);
-////            TextView t2v = new TextView(this);
-////            t2v.setText("Product " + i);
-////            t2v.setTextColor(Color.WHITE);
-////            t2v.setGravity(Gravity.CENTER);
-////            tbrow.addView(t2v);
-////            TextView t3v = new TextView(this);
-////            t3v.setText("Rs." + i);
-////            t3v.setTextColor(Color.WHITE);
-////            t3v.setGravity(Gravity.CENTER);
-////            tbrow.addView(t3v);
-////            TextView t4v = new TextView(this);
-////            t4v.setText("" + i * 15 / 32 * 10);
-////            t4v.setTextColor(Color.WHITE);
-////            t4v.setGravity(Gravity.CENTER);
-////            tbrow.addView(t4v);
-////            stk.addView(tbrow);
-//        }
-
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        dexGen();
         init();
-
-
     }
-
-
 }
